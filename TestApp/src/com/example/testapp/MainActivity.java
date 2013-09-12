c8da9697e5a9e381c577d8ae221ed834ff2efe1b
Krunal Doshi <krunal.etechmavens@gmail.com>
2013-09-12 18:29:06 +0530

    added image display code.

--- TestApp/src/com/example/testapp/MainActivity.java
+++ TestApp/src/com/example/testapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.testapp;
 
-import android.os.Bundle;
+import java.io.File;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +19,11 @@ public class MainActivity extends Activity {
 		// this is added after importing from git and than in eclipse
 		
 		// this is added after one commite in local
+		
+		File f = new File("/mnt/sdcard/url.jpg");
+		ImageView mImgView1 = (ImageView)findViewById(R.id.imageView);
+		Bitmap bmp = BitmapFactory.decodeFile(f.getAbsolutePath());
+		mImgView1.setImageBitmap(bmp);
 	}
 
 	
