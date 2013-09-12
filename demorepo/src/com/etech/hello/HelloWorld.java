d9d25a9f57f771880e7a1ba1801699be6e494d42
eTechMavens Developer <developer@etechmavens.com>
2013-09-12 13:14:16 +0530

    Added Commnets to main method

--- demorepo/src/com/etech/hello/HelloWorld.java
+++ demorepo/src/com/etech/hello/HelloWorld.java
@@ -1,5 +1,10 @@
 package com.etech.hello;
 
+/**
+ * This is the main method called when program runs
+ * @author nigam
+ *
+ */
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hi This is Nigam Vakharia");
